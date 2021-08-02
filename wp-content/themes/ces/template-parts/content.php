7d079d5f1d8904cebbe87855483303bb8865907b
Jono L <contact@cityeditionstudio.co.uk>
2021-08-02 20:46:15 +0100

    added post buttons

--- wp-content/themes/ces/template-parts/content.php
+++ wp-content/themes/ces/template-parts/content.php
@@ -11,24 +11,34 @@
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-<section>
+<section id="anchorimages" >
 
 	<div class="hero">
 		<div class="w-100 vh-100 cover bg-center">
-		<?php if( get_field('desktop_slider') ): ?>
-		<?php the_field('desktop_slider'); ?>
-		<?php endif; ?>
+			<?php if( get_field('desktop_slider') ): ?>
+			<?php the_field('desktop_slider'); ?>
+			<?php endif; ?>
 		</div>	
+		<div class="downbutton">
+			<a href="#anchortext"><h2>Words <span>&#8595;</span> </h2></a>
+		</div>
 	</div>
 
-	<div class="min-vh-100 flex items-center justify-center pv5">
+</section>
+
+<section id="anchortext" >
+
+	<div  class="min-vh-100 flex items-center justify-center pv5">
 		<div class="measure-wide center tc">
 			<h1 id="post_title"><?php the_title(); ?></h1>
 
 			<?php if( get_field('blurb') ): ?>
 			<p class="f4 lh-copy"><?php the_field('blurb'); ?></p>
 			<?php endif; ?>
-
+			
+			<div class="upbutton">
+				<a href="#anchorimages"><h2>Images <span>&#8593;</span> </h2></a>
+			</div>
 		</div>
 	</div>
 
