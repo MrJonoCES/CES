812880e9a4840a2115c6285b4a3a73d6d09e7268
Jono L <contact@cityeditionstudio.co.uk>
2021-12-05 09:23:54 +0000

    vid div fix

--- wp-content/themes/ces/information.php
+++ wp-content/themes/ces/information.php
@@ -62,9 +62,9 @@ get_header();
 
 		<section class="mix-panel" id="mix" >
 
-				<div>
-				<video class="vid-slider" controls="" name="media"><source src="https://www.dropbox.com/s/f5snduipzt3cylg/Lockdown%20Records%20Mix.mp4?raw=1" type="video/mp4"></video>
-				</div>
+				<div class="vid-slider">
+				<script src="https://fast.wistia.com/embed/medias/f9cit07pqo.jsonp" async></script><script src="https://fast.wistia.com/assets/external/E-v1.js" async></script><div class="wistia_responsive_padding" style="padding:56.25% 0 0 0;position:relative;"><div class="wistia_responsive_wrapper" style="height:100%;left:0;position:absolute;top:0;width:100%;"><div class="wistia_embed wistia_async_f9cit07pqo videoFoam=true" style="height:100%;position:relative;width:100%"><div class="wistia_swatch" style="height:100%;left:0;opacity:0;overflow:hidden;position:absolute;top:0;transition:opacity 200ms;width:100%;"><img src="https://fast.wistia.com/embed/medias/f9cit07pqo/swatch" style="filter:blur(5px);height:100%;object-fit:contain;width:100%;" alt="" aria-hidden="true" onload="this.parentNode.style.opacity=1;" /></div></div></div></div>
+				</div
 
 		</section>
 
